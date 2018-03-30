226c9781ce067a758617baf792f856d31b363fd6
PODEROSISSIMA\Adriano Gonçalves <driko.argon@gmail.com>
2018-03-29 22:07:47 -0300

    modanças no tutorial

---
+++ Assets/Scripts/Scenario/TutorialPit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPit : MonoBehaviour {
+
+	public GameObject returnPoint;
+
+	void OnCollisionEnter2D(Collision2D other){
+		
+		if(other.gameObject.GetComponent<Cat>() != null){
+			GameObject cat = other.gameObject;
+			cat.transform.position = returnPoint.transform.position;
+		}
+	}
+}
--- Assets/Scripts/Scenario/TutorialPortal.cs
+++ Assets/Scripts/Scenario/TutorialPortal.cs
@@ -6,11 +6,29 @@ using UnityEngine.SceneManagement;
 public class TutorialPortal : MonoBehaviour {
 
 	public int destinationSceneIndex;
+	public KeyCode enterPortalKey;
+	public string enterPortalGamepadButton;
 
+	private bool catIsNear;
+
+	void Update(){
+		if(catIsNear){
+			if(Input.GetKeyDown (enterPortalKey) || Input.GetButtonDown(enterPortalGamepadButton)){
+				Enter();
+			}
+		}
+	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.gameObject.tag == "Player"){
-			Enter();
+			catIsNear = true;
+		}
+
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.gameObject.tag == "Player"){
+			catIsNear = false;
 		}
 
 	}
