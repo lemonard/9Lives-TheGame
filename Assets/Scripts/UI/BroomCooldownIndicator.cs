6f98c7fed0079c9371d60fab6906bfa9ea8ca72d
PODEROSISSIMA\Adriano Gonçalves <driko.argon@gmail.com>
2018-04-01 23:25:37 -0300

    Timeline do Sebastian Terminando a Fase e Broom Cooldown Indicator

---
+++ Assets/Scripts/UI/BroomCooldownIndicator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BroomCooldownIndicator : MonoBehaviour {
+
+	public static BroomCooldownIndicator instance;
+	
+	public Image skillImage;
+	public float cooldownTime;
+	public bool onCooldown;
+
+	private float fillAmount;
+
+	void Awake(){
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		skillImage.fillAmount = 1;
+		fillAmount = 1;
+	}
+
+	void Update(){
+		if(onCooldown){
+
+			fillAmount += Time.deltaTime;
+			skillImage.fillAmount = fillAmount/cooldownTime;
+
+			if(skillImage.fillAmount >= 1f){
+				onCooldown = false;
+			}
+		}
+
+	}
+
+
+	public void StartCooldown(){
+		fillAmount = 0;
+		skillImage.fillAmount = 0;
+		onCooldown = true;
+
+	}
+
+
+}
